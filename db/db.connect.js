881731cae828dad173014944ae4f88d18168996d
Aman Khare <khareaman754@gmail.com>
2025-02-25 11:13:05 +0530

    chore increase mongoose connect server time

--- db/db.connect.js
+++ db/db.connect.js
@@ -4,7 +4,10 @@ dotenv.config({ path: ".env" });
 
 const initializeDatabase = async () => {
   try {
-    const dbConnect = await mongoose.connect(process.env.MONGO_URI);
+    const dbConnect = await mongoose.connect(process.env.MONGO_URI, {
+      serverSelectionTimeoutMS: 5000, // 5 seconds
+      socketTimeoutMS: 45000, // 45 seconds
+    });
 
     if (dbConnect) console.log("Connected to mongoDB");
   } catch (error) {
